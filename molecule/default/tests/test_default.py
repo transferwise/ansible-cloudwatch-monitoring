2e99e79da296f2c559cb161ea61ead76df47d0a4
James Bach <jalev@users.noreply.github.com>
2019-01-17 11:17:24 +0000

    Ubuntu Compatibility  (#1)
    * initial commit
    
    * General reformatting to make it look pretty
    
    * Molecule Tests (#3)
    
    * change include_tasks to just include
    
    * Move installation of dependencies into own file
    
    * Break the configuration into its own file
    
    * Moves the zip install to install-prereqs
    
    * Turns the zip location into a variable
    
    * Encapsulate variables for configure task
    
    * Make things look nicer
    
    * Adds molecule tests
    
    * Fix yamllint problems
    
    * Fix flake8 lint
    
    * Fix ansible lint problems
    
    * removes pyc
    
    * Fix tests
    
    * Removes travis for internal circle (#4)

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,35 @@
+import os
+import testinfra.utils.ansible_runner
+
+# uses https://testinfra.readthedocs.io/en/latest/modules.html#modules
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_setup_file(host):
+    f = host.file('/tmp/CloudWatchMonitoringScripts.zip')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_script_directory(host):
+    f = host.file('/opt/aws-scripts-mon')
+
+    assert f.exists
+    assert f.is_directory
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_script_file(host):
+    f = host.file('/opt/aws-scripts-mon/cw-put-instance-data.sh')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.contains('export')
+    assert f.contains('--disk-space-util')
+    assert f.contains('/opt/aws-scripts-mon/')
